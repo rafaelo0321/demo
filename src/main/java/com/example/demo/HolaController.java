9c3df6c99e546072d49a15889734c0d20586b758
rafaelo0321 <rafaninho21@gmail.com>
2024-10-03 20:38:42 -0500

    add: Controlador de prueba endpoint hola mundo

---
+++ src/main/java/com/example/demo/HolaController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hola")
+public class HolaController {
+    @GetMapping
+    public ResponseEntity<String> holaMundo(){
+        return ResponseEntity.ok("Hola Rafael bienvenido");
+    }
+}
